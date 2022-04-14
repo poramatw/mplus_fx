98989b6b46458583214c5fa7f215e984655919d2
Poramat Wongtawan <103097276+poramatw@users.noreply.github.com>
2022-04-14 15:56:44 +0700

    Update Ui accountDetails
    change icon and color in box services

--- lib/screens/accountDetails.dart
+++ lib/screens/accountDetails.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-import 'package:mplus_fx/screens/viewDetails.dart';
+//import 'package:mplus_fx/screens/viewDetails.dart';
+import 'package:mplus_fx/widgets/cardViewdetails.dart';
 
 class AccountDetails extends StatefulWidget {
   const AccountDetails({Key? key}) : super(key: key);
@@ -11,210 +12,192 @@ class AccountDetails extends StatefulWidget {
 }
 
 class _AccountDetailsState extends State<AccountDetails> {
+  Color amberC = Color.fromRGBO(255, 193, 79, 1);
+  Color mainColor = Color.fromRGBO(22, 29, 111, 1);
+  Color boxColor = Color.fromARGB(169, 158, 159, 162);
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(children: [
         Container(
-          //decoration: BoxDecoration(borderRadius: BorderRadius.circular(22)),
+          //decoration: BoxDecoration(borderRadius: BorderRadius.),
           color: Color.fromRGBO(22, 29, 111, 1),
           height: size.height * 0.33,
         ),
-        ListView(
-          physics: ClampingScrollPhysics(),
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 16, top: 32, right: 30, bottom: 33),
-              child: Container(
-                  //margin: EdgeInsets.only(left: 16, right: 26, top: 32),
-                  child: Row(
-                children: [
-                  IconButton(
-                      color: Colors.white,
-                      iconSize: 30,
-                      icon: Icon(Icons.navigate_before),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      }),
-                  Text(
-                    "MplusFX",
-                    style: TextStyle(
+        Container(
+          height: size.height,
+          width: size.width,
+          child: ListView(
+            physics: ClampingScrollPhysics(),
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16, top: 32, right: 30, bottom: 33),
+                child: Container(
+                    //margin: EdgeInsets.only(left: 16, right: 26, top: 32),
+                    child: Row(
+                  children: [
+                    IconButton(
                         color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 39,
-                    width: 39,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/logo.png'))),
-                  ),
-                ],
-              )),
-            ),
-            //CardView
-
-            Padding(
-                padding: EdgeInsets.only(
-                  left: 30,
-                ),
-                child: Text(
-                  "Accounts",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
+                        iconSize: 30,
+                        icon: Icon(Icons.navigate_before),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        }),
+                    Text(
+                      "MplusFX",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Spacer(),
+                    CircleAvatar(
+                      child: Icon(Icons.person_off_outlined),
+                    )
+                  ],
                 )),
-            SizedBox(
-              height: 42,
-            ),
-            Container(
-              height: 205,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(left: 15, right: 15),
-                  itemCount: 2,
-                  itemBuilder: (context, index) {
-                    return Container(
-                        margin: EdgeInsets.only(right: 15, left: 15),
-                        height: 205,
-                        width: 200,
+              ),
+              //CardView
+              Padding(
+                  padding: EdgeInsets.only(
+                    left: 30,
+                  ),
+                  child: Text(
+                    "Accounts",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )),
+              SizedBox(
+                height: 42,
+              ),
+              CardViewdetails("Standard", "123456789", 2000.59, amberC),
+/////////////////////////////////////////////////////////////////////////////////////////////
+              Padding(
+                padding: const EdgeInsets.only(left: 39, top: 22),
+                child: Text(
+                  "SERVICES",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromRGBO(27, 29, 111, 1)),
+                ),
+              ),
+              ///////////////////////////////////////////////////////////////////////////////////////////////////
+              Padding(
+                padding: const EdgeInsets.only(left: 30, top: 20),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.greenAccent),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                                left: 30,
-                                top: 40,
-                                child: Text(
-                                  "MT4 Standard Account",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Color.fromRGBO(27, 29, 111, 1),
-                                      fontWeight: FontWeight.w600),
-                                )),
-                            Positioned(
-                                left: 30,
-                                top: 64,
-                                child: Text(
-                                  "123456789",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Color.fromRGBO(27, 29, 111, 1),
-                                      fontWeight: FontWeight.w600),
-                                )),
-                            Positioned(
-                                left: 24,
-                                top: 90,
-                                child: Text(
-                                  "2,000.00 USD",
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      color: Color.fromRGBO(27, 29, 111, 1),
-                                      fontWeight: FontWeight.w600),
-                                )),
-                            Positioned(
-                                left: 30,
-                                top: 145,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(context,
-                                        MaterialPageRoute(builder: (context) {
-                                      return ViewDetials();
-                                    }));
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(30),
-                                        color: Colors.white),
-                                    height: 40,
-                                    width: 144,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          "View Details",
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.w600,
-                                              color: Color.fromRGBO(
-                                                  27, 29, 111, 1)),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                )),
-                          ],
-                        ));
-                  }),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 39, top: 22),
-              child: Text(
-                "SERVICES",
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                    color: Color.fromRGBO(27, 29, 111, 1)),
+                            color: boxColor,
+                            borderRadius: BorderRadius.circular(30)),
+                        height: 145,
+                        width: 145,
+                        child: Column(children: [
+                          SizedBox(
+                              height: 106,
+                              width: 35,
+                              child: Icon(Icons.account_balance_outlined)),
+                          Text(
+                            "ฝากเงิน",
+                            style: TextStyle(
+                                color: mainColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ]),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: boxColor,
+                            borderRadius: BorderRadius.circular(30)),
+                        height: 145,
+                        width: 145,
+                        child: Column(children: [
+                          SizedBox(
+                              height: 106,
+                              width: 35,
+                              child: Icon(Icons.account_balance_outlined)),
+                          Text(
+                            "ถอนเงิน",
+                            style: TextStyle(
+                                color: mainColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ]),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, top: 20),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 156, 168, 203),
-                        borderRadius: BorderRadius.circular(30)),
-                    height: 145,
-                    width: 145,
-                    child: Column(children: []),
-                  ),
-                  SizedBox(
-                    width: 25,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 156, 168, 203),
-                        borderRadius: BorderRadius.circular(30)),
-                    height: 145,
-                    width: 145,
-                    child: Column(children: []),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, top: 30, bottom: 30),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 156, 168, 203),
-                        borderRadius: BorderRadius.circular(30)),
-                    height: 145,
-                    width: 145,
-                    child: Column(children: []),
-                  ),
-                  SizedBox(
-                    width: 25,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 156, 168, 203),
-                        borderRadius: BorderRadius.circular(30)),
-                    height: 145,
-                    width: 145,
-                    child: Column(children: []),
-                  ),
-                ],
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 30, top: 30, bottom: 30),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: boxColor,
+                            borderRadius: BorderRadius.circular(30)),
+                        height: 145,
+                        width: 145,
+                        child: Column(children: [
+                          SizedBox(
+                              height: 106,
+                              width: 35,
+                              child: Icon(Icons.account_balance_outlined)),
+                          Text(
+                            "โอนเงิน",
+                            style: TextStyle(
+                                color: mainColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ]),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: boxColor,
+                            borderRadius: BorderRadius.circular(30)),
+                        height: 145,
+                        width: 145,
+                        child: Column(children: [
+                          SizedBox(
+                              height: 106,
+                              width: 35,
+                              child: Icon(Icons.account_balance_outlined)),
+                          Text(
+                            "UTC SERVICE",
+                            style: TextStyle(
+                                color: mainColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ]),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ]),
     );
---
+++ lib/widgets/cardViewdetails.dart
@@ -0,0 +1,102 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../screens/viewDetails.dart';
+
+class CardViewdetails extends StatelessWidget {
+  String accType;
+  String accNum;
+  double accCredit;
+  Color color;
+  Color mainColor = Color.fromRGBO(22, 29, 111, 1);
+
+  CardViewdetails(this.accType, this.accNum, this.accCredit, this.color,
+      {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 205,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.only(left: 15, right: 15),
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return Container(
+                margin: EdgeInsets.only(right: 15, left: 15),
+                height: 205,
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30), color: color),
+                child: Stack(
+                  children: [
+                    Positioned(
+                        left: 30,
+                        top: 40,
+                        child: Text(
+                          "MT4 " + accType + " Account",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Color.fromRGBO(27, 29, 111, 1),
+                              fontWeight: FontWeight.w600),
+                        )),
+                    Positioned(
+                        left: 30,
+                        top: 64,
+                        child: Text(
+                          accNum,
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Color.fromRGBO(27, 29, 111, 1),
+                              fontWeight: FontWeight.w600),
+                        )),
+                    Center(
+                      child: Text(
+                        '${NumberFormat("#,###.##").format(accCredit)}'
+                        " USD",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Color.fromRGBO(27, 29, 111, 1),
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Positioned(
+                        left: 30,
+                        top: 145,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return ViewDetials();
+                            }));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.white),
+                            height: 40,
+                            width: 144,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Text(
+                                  "View Details",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                      color: mainColor),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )),
+                  ],
+                ));
+          }),
+    );
+  }
+}
